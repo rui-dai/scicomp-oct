b2bb6f7582dbb8a869f3250d5bc9e10fdb2bde39
Rui Dai <rui.dai.mail@gmail.com>
2016-10-18 10:07:53 -0400

    Prints usage if no file provided
    this must have an argument. stuff happens

--- read_fasta.py
+++ read_fasta.py
@@ -1,3 +1,5 @@
+import sys
+
 def read_fasta(filename):
     sequence = ''
     f = open(filename)
@@ -9,4 +11,8 @@ def read_fasta(filename):
     f.close()
     return sequence
 
-print read_fasta('ae.fa')
+if len(sys.argv) < 2:
+    print "Usage:", sys.argv[0], "<sequence.fa>"
+    exit(1)
+
+print read_fasta(sys.argv[1])
